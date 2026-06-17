2fcd7be8fec87e85ab9d160a817f4caa87e4ad3e
mounanaji810-star <mounanaji810@gmail.com>
2026-06-17 17:52:55 +0000

    Delete InterventionStatus.java

--- InterventionStatus.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public enum InterventionStatus {
-    EN_ATTENTE,  // ✅ créé, pas encore démarré
-    EN_COURS,    // ✅ démarré → dateDebut automatique
-    TERMINEE,    // ✅ terminé → dateFin automatique
-    ANNULEE
-}
